b0ff7eade11d4ea8a6795e4cb86e2b5ea0127481
ZeeAlgo <ahmadfauzi240106@gmail.com>
2025-07-31 10:02:29 +0700

    nambah dashboard

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/users', function () {
+    return view('users.index');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
